45028c2b69a9bf5802954e746b4af33a621e93c8
connection579 <wlswn4118@naver.com>
2020-11-07 14:04:58 +0900

    조건연산자의 참과 거짓 증명 하기

---
+++ src/operationTest5/OperationTest5.java
@@ -0,0 +1,17 @@
+package operationTest5;
+
+public class OperationTest5 {
+
+	public static void main(String[] args) {
+		int fatherAge = 45;
+		int motherAge = 47;
+		
+		char ch ;
+		ch = (fatherAge>motherAge) ? 'T' : 'F';
+		
+		System.out.print(ch);
+		
+
+	}
+
+}
